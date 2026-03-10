b5988c4ff0299eef0f9eaad00b1bf8487fe9f60b
aleksejtutynin <aleksej-tutynin@mail.ru>
2026-03-11 00:21:24 +0300

    add order_builder.h skeleton

---
+++ C++/order_builder.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "coffee_order.h"
+
+// Классы: PriceCalculator, OrderBuilder
+
+class PriceCalculator {
+public:
+    PriceCalculator(std::vector<ProductVariant> variants,
+                    std::vector<Modifier> modifiers);
+
+private:
+    std::vector<ProductVariant> variants_;
+    std::vector<Modifier> modifiers_;
+};
+
+class OrderBuilder {
+public:
+    OrderBuilder() = default;
+
+private:
+    CoffeeOrder order_;
+};
